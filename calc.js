e47bf5cdcbf8ca7d16abbf278708488a15bbd232
Peter Limbu <peterlimbu@spartaglobal.com>
2018-07-01 16:30:19 +0100

    add read me file, add advanced calculator function, add game loop

--- calc.js
+++ calc.js
@@ -4,6 +4,7 @@ console.log("hello world");
 //main() variables
 var name;
 var loop = true;
+var userchoice;
 
 //calc variables
 var number_1;
@@ -11,6 +12,9 @@ var number_2;
 var answ;
 var operator;
 
+//loop begins
+while(loop === true)
+{
 
 //prompt to ask if they would like to use basic calculator or advanced calulcator
 name = prompt("Type (b) to run basic calculator and (a) to run advanced calculator");
@@ -32,8 +36,8 @@ break;
 //advanced calculator
   case "a":
 
-//user inputs operator choice
-operator = prompt("What operator would you like to use ?\n power = (1)\n square root = (2) ");
+//user inputs operator choice, used 5 and 6 instead of 1 and 2 so that we didn't need to use 2 switch statements, this was a design choice made to make the code more effecient
+operator = prompt("What operator would you like to use ?\n power = (5)\n square root of first number user enters = (6) ");
 
   //get number input
   number_1 = prompt("enter number 1");
@@ -78,8 +82,28 @@ break;
 //advanced calculator
 
 
+//power
+case "5":
+answ = Math.pow(+number_1, +number_2)
+alert(number_1+" ^ "+number_2 +" = "+answ);
+break;
 
+//root
+case "6":
+answ = Math.sqrt(+number_1);
+alert( "squareroot("+number_1 + ") = "+answ);
+break;
 
 
 
 }
+
+userchoice = prompt("type (y) to use caclulator again, else press (n) to exit")
+if(userchoice == "n")
+{
+  loop = false;
+
+}
+
+
+}//end of loop
